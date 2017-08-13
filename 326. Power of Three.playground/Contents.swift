ad54c9b98f620504a0520b6b85e8f7706ef9762d
Dave Alton <dave@ovuline.com>
2017-08-12 21:22:43 -0700

    Add 326. Power of Three

---
+++ 326. Power of Three.playground/Contents.swift
@@ -0,0 +1,18 @@
+/*
+ Given an integer, write a function to determine if it is a power of three.
+ 
+ Follow up:
+ Could you do it without using any loop / recursion?
+ */
+
+import Darwin
+
+class Solution {
+    func isPowerOfThree(_ n: Int) -> Bool {
+        guard n > 0 else { return false }
+        return (1162261467.0).remainder(dividingBy: Double(n)) == 0
+    }
+}
+
+Solution().isPowerOfThree(27)
+Solution().isPowerOfThree(-3)
